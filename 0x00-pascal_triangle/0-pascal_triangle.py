3a92dc87d2323e7f359715ebbd08f6ef538a4bc7
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2022-11-06 18:28:15 +0100

    fix: function docs and formating

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,28 @@
+# This function accepts an integer (n)
+# return a list containing the pascal triangle of n
 def pascal_triangle(n):
     # empty list to store triangles
-    triangle =[]
-    
+    triangle = []
+
     # return an empty list, if n is less than 1
     if(n<=0): return triangle
-    
+
     # Generage n number of rows 
     for i in range(n):
         # calculate each entry in the triangle with "n choose k" formula 
-        # C(n,c) or n!/k!*(n-k)!
-        # "!" means factorial: products of series of descending natural numbers of a given number.
+        # C(n,c) or n!/k!*(n-k)! 
+        # ref: https://www.mathsisfun.com/pascals-triangle.html
+        # "!" means factorial: products of a series of 
+        #descending natural numbers of a given number.
         triangle.append([factorial(i)//(factorial(k)*factorial(i-k)) for k in range(i+1)])
     return triangle
-    
+
+# This function accepts an integer n and returns a factorial value of n
 def factorial(n):
     fact = 1
     if(n==0):
         return 1
-    while(n>0):
-        fact = n*fact
-        n-=1
+    while(n > 0):
+        fact = n * fact
+        n -= 1
     return fact
