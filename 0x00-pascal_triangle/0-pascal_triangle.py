4aa3190cf2327c92abaa3fd240de004e1c06ca13
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2022-11-07 11:05:14 +0100

    fix: PEP8 style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,8 @@
 ''' This module generate a list of pascal triangle
 given an integer.
 '''
+
+
 def pascal_triangle(n):
     '''This function accepts an integer (n)
     return a list containing the pascal triangle of n'''
@@ -16,20 +18,20 @@ def pascal_triangle(n):
     # Generage n number of rows 
     for i in range(n):
         # calculate each entry in the triangle with "n choose k" formula 
-        # C(n,c) or n!/k!*(n-k)! 
-        # ref: https://www.mathsisfun.com/pascals-triangle.html
+        # C(n,c) or n! / k! * (n - k)! 
         # "!" means factorial: products of a series of 
         #descending natural numbers of a given number.
-        triangle.append([factorial(i)//(factorial(k)*factorial(i-k)) for k in range(i+1)])
+        row = [fact(i) // (fact(k) * fact(i - k)) for k in range(i + 1)]
+        triangle.append(row)
     return triangle
 
 
-def factorial(n):
+def fact(n):
     '''This function accepts an integer n and returns a factorial value of n'''
-    fact = 1
+    factorial = 1
     if(n==0):
         return 1
     while(n > 0):
-        fact = n * fact
+        factorial = n * factorial
         n -= 1
-    return fact
+    return factorial
