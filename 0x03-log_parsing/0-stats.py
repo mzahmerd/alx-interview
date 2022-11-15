986d23258241e8427b5dc183fcbf8f5958519f5c
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2022-11-15 23:15:49 +0100

    refactor: functions reposition

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,33 +5,36 @@ import re
 parse and print out some stats'''
 
 
+def match_format(line=""):
+    '''Check if the line match required format using regex'''
+
+    return re.match(
+        r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{1,4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}.\d*\] \"GET /projects/260 HTTP/1.1\" (200|301|400|401|403|404|405|500) \d+$", line
+    )
+
+
+def log_line(line):
+    '''Take line and take necessary data for statistics'''
+    line = line[:-1]
+    words = line.split(" ")
+    file_size[0] += int(words[-1])
+    code = int(words[-2])
+    if code in codes:
+        codes[code] += 1
+
+
+def print_stats():
+    '''print statistics of current files'''
+    print("File size: {}".format(file_size))
+    for code in codes.keys():
+        if codes[code]:
+            print("{}: {}".format(code, codes[code]))
+
+
 if __name__ == "__main__":
     file_size = [0]
     codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     count = 1
-
-    def match_format(line=""):
-        '''Check if the line match required format using regex'''
-
-        return re.match(
-            r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{1,4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}.\d*\] \"GET /projects/260 HTTP/1.1\" (200|301|400|401|403|404|405|500) \d+$", line
-        )
-
-    def log_line(line):
-        '''Take line and take necessary data for statistics'''
-        line = line[:-1]
-        words = line.split(" ")
-        file_size[0] += int(words[-1])
-        code = int(words[-2])
-        if code in codes:
-            codes[code] += 1
-
-    def print_stats():
-        '''print statistics of current files'''
-        print("File size: {}".format(file_size))
-        for code in codes.keys():
-            if codes[code]:
-                print("{}: {}".format(code, codes[code]))
 
     try:
         for line in sys.stdin:
