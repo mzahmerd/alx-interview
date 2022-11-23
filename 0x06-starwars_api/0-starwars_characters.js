f0dacc82f0c15d484993de178914c7d181321dd8
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2022-11-24 00:03:22 +0100

    style standard and exec

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,10 +1,12 @@
-const request=require('request')
+#!/usr/bin/node
+const request=require ('request')
 
-const charachters=[]
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,(err,res,body) => {
-    if(err) return console.log(err)
-    if(res&&res.statusCode) {
-        const charactersApi=JSON.parse(body).characters
-        charactersApi.map(charApi => request(charApi,(err,res,data) => console.log(JSON.parse(data).name)))
+const charachters = []
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}` , (err, res, body) => {
+    if (err) return console.log(err)
+    if (res&&res.statusCode) {
+        const charactersApi = JSON.parse(body).characters
+        charactersApi.map(charApi => request(charApi, (err, res, data) => console.log(JSON.parse(data).name)))
     }
 })
+
