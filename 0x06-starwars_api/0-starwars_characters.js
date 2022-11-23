f7ebc6fecad9fd9f5910a25bd6d880a591c99e1c
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2022-11-23 23:35:34 +0100

    feat: Star Wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,10 @@
+const request=require('request')
+
+const charachters=[]
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,(err,res,body) => {
+    if(err) return console.log(err)
+    if(res&&res.statusCode) {
+        const charactersApi=JSON.parse(body).characters
+        charactersApi.map(charApi => request(charApi,(err,res,data) => console.log(JSON.parse(data).name)))
+    }
+})
