3f7ebdbc99b7c6d26cd3ebc815e77b18fcb238b9
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2022-11-27 19:00:51 +0100

    fix: used recursion

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,12 +1,26 @@
 #!/usr/bin/node
-const request=require ('request')
+const request=require('request')
 
-const charachters = []
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}` , (err, res, body) => {
-    if (err) return console.log(err)
-    if (res&&res.statusCode) {
-        const charactersApi = JSON.parse(body).characters
-        charactersApi.map(charApi => request(charApi, (err, res, data) => console.log(JSON.parse(data).name)))
+const charachters=[]
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,async (err,res,body) => {
+    if(err) {
+        return
+        // console.log(err)
+    } else {
+        // const charactersApi=JSON.parse(body).characters
+        printCharacter(JSON.parse(body).characters,0)
     }
 })
 
+const printCharacter=(charApi,index) => {
+    if(index===charApi.length) return
+    request(charApi[index],(err,res,data) => {
+        if(err) {
+            console.log(err)
+        } else {
+            console.log(JSON.parse(data).name)
+            index+=1
+            printCharacter(charApi,index)
+        }
+    })
+}
